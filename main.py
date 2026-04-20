785c1baf26cdaa752ed04a851e6a8b05300f4f37
Sosna123 <olek.arez@gmail.com>
2026-04-20 20:41:46 +0200

    now the bot adds words to the files guildid.txt

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+prefix = "\\"
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -17,7 +19,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(f'{prefix}add'):
+        messageWithoutAdd = message.content.split(" ")
+        messageWithoutAdd.pop(0)
+        result = " ".join(messageWithoutAdd)
+
+        file = open(f"./lists/{message.guild.id}.txt", "a")
+        file.write(result + "\n")
+        file.close()
+
+        await message.channel.send(f"Added {result} to the list!")
 
 client.run(os.getenv('BOT_TOKEN'))
