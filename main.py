2c9ff5085239bbd4b30f8dac8a384ec3ffef213f
Sosna123 <olek.arez@gmail.com>
2026-04-20 21:03:12 +0200

    now the bot can show all the words in a message and moved some stuff to functions

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ client = discord.Client(intents=intents)
 
 prefix = "\\"
 
+def getListOfWords(guildId):
+    file = open(f"./lists/{guildId}.txt", "r")
+    words = file.readlines()
+
+    for i in range(len(words)):
+        words[i] = words[i].strip()
+
+    return words
+
+def addToFile(word, guildId):
+    list = getListOfWords(guildId)
+
+    if(len(word) > 32):
+        return 1
+    if(list.count(word.lower()) > 0):
+        return 2
+    else:
+        file = open(f"./lists/{guildId}.txt", "a")
+        file.write(word.lower() + "\n")
+        file.close()
+        return 0
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -19,15 +41,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+
     if message.content.startswith(f'{prefix}add'):
         messageWithoutAdd = message.content.split(" ")
         messageWithoutAdd.pop(0)
-        result = " ".join(messageWithoutAdd)
+        word = " ".join(messageWithoutAdd)
 
-        file = open(f"./lists/{message.guild.id}.txt", "a")
-        file.write(result + "\n")
-        file.close()
+        errors = addToFile(word, message.guild.id)
 
-        await message.channel.send(f"Added {result} to the list!")
+        if(errors == 1):
+            await message.channel.send(f"{word} has more than 32 characters!")
+        if(errors == 2):
+            await message.channel.send(f"{word} has already been added!")
+        else:
+            await message.channel.send(f"Added {word} to the list!")
+
+
+    if message.content.startswith(f'{prefix}show'):
+        wordsList = getListOfWords(message.guild.id)
+        await message.channel.send(f"Your words (there is {len(wordsList)} of them): \n ```{",".join(wordsList)}```")
 
 client.run(os.getenv('BOT_TOKEN'))
